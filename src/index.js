94356db4ec469f636d161b36a91a91b3c2916e0c
Dvent1123 <58802270+Dvent1123@users.noreply.github.com>
2020-11-04 14:20:50 -0700

    Init js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDom from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//css
+import './index.css'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function BookList() {
+    return (
+        <section className='booklist'>
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+        </section>
+    )
+}
+
+const Book = () => {
+    return <article className="book">
+        <Image/>
+        <Title/>
+        <Author/>
+    </article>
+}
+
+const Title = () => <h1>The Housekeeper: A Twisted Psychological Thriller</h1>
+
+const Image = () => <img src="https://m.media-amazon.com/images/I/81BC-VqUZfL._AC_UL480_FMwebp_QL65_.jpg" alt=""/>
+
+const Author = () => <h4>Natalie Barelli</h4>
+
+ReactDom.render(<BookList/>, document.getElementById('root'))
