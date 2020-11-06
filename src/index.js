3c6dc34cea254332e631bf4d325b7f5c5dd99c98
Dvent1123 <58802270+Dvent1123@users.noreply.github.com>
2020-11-05 18:14:56 -0700

    Added props

--- src/index.js
+++ src/index.js
@@ -1,35 +1,44 @@
-import React from 'react'
+import React, { Children } from 'react'
 import ReactDom from 'react-dom'
 
 //css
 import './index.css'
 
+//setup vars
+const firstBook = {
+    img: 'https://m.media-amazon.com/images/I/81BC-VqUZfL._AC_UL480_FMwebp_QL65_.jpg',
+    author: 'Natalie Barelli',
+    title: 'The Housekeeper: A Twisted Psychological Thriller'
+}
+
+const secondBook = {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/91amG4aECWL._AC_UL200_SR200,200_.jpg',
+    author: 'Dr seuss',
+    title: 'What Was I Scared Of?'
+}
+
+
 function BookList() {
     return (
         <section className='booklist'>
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
+            <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum pariatur dolore, 
+                    sequi voluptatum est odio quisquam a animi mollitia voluptatem.</p>
+            </Book>
+            <Book img={secondBook.img} title={secondBook.title} author={secondBook.author}/>
         </section>
     )
 }
 
-const Book = () => {
+const Book = (props) => {
+    console.log(props)
     return <article className="book">
-        <Image/>
-        <Title/>
-        <Author/>
+        <img src={props.img} alt=""/>
+        <h1>{props.title}</h1>
+        <h4>{props.author}</h4>
+        {props.children}
     </article>
 }
 
-const Title = () => <h1>The Housekeeper: A Twisted Psychological Thriller</h1>
-
-const Image = () => <img src="https://m.media-amazon.com/images/I/81BC-VqUZfL._AC_UL480_FMwebp_QL65_.jpg" alt=""/>
-
-const Author = () => <h4>Natalie Barelli</h4>
 
 ReactDom.render(<BookList/>, document.getElementById('root'))
