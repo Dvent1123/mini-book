2fbe53ed58e76dbca64956d51fc2cc5ad8309463
Dvent1123 <58802270+Dvent1123@users.noreply.github.com>
2020-11-08 16:27:41 -0700

    Importing and exporting

---
+++ src/books.js
@@ -0,0 +1,21 @@
+export const books = [
+    {
+    id:1,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81Me-r4UL1L._AC_UL200_SR200,200_.jpg',
+    author: 'Natalie Barelli',
+    title: 'The Housekeeper: A Twisted Psychological Thriller'
+},
+{
+    id:2,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/91amG4aECWL._AC_UL200_SR200,200_.jpg',
+    author: 'Dr seuss',
+    title: 'What Was I Scared Of?'
+},
+{
+    id:3,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71Ks%2B8mKq2L._AC_UL200_SR200,200_.jpg',
+    author: 'Jeff Kinney',
+    title: 'The Deep End (Diary of a Wimpy Kid Book'
+}
+
+]
--- src/index.js
+++ src/index.js
@@ -3,30 +3,9 @@ import ReactDom from 'react-dom'
 
 //css
 import './index.css'
-
+import {books} from './books'
+import Book from './Book'
 //setup vars
-
-const books = [
-    {
-    id:1,
-    img: 'https://images-na.ssl-images-amazon.com/images/I/81Me-r4UL1L._AC_UL200_SR200,200_.jpg',
-    author: 'Natalie Barelli',
-    title: 'The Housekeeper: A Twisted Psychological Thriller'
-},
-{
-    id:2,
-    img: 'https://images-na.ssl-images-amazon.com/images/I/91amG4aECWL._AC_UL200_SR200,200_.jpg',
-    author: 'Dr seuss',
-    title: 'What Was I Scared Of?'
-},
-{
-    id:3,
-    img: 'https://images-na.ssl-images-amazon.com/images/I/71Ks%2B8mKq2L._AC_UL200_SR200,200_.jpg',
-    author: 'Jeff Kinney',
-    title: 'The Deep End (Diary of a Wimpy Kid Book'
-}
-
-]
 
 
 function BookList() {
@@ -39,14 +18,6 @@ function BookList() {
     )
 }
 
-const Book = (props) => {
-    const {img, title, author} = props;
-    return <article className="book">
-        <img src={img} alt=""/>
-        <h1>{title}</h1>
-        <h4>{author}</h4>
-    </article>
-}
 
 
 ReactDom.render(<BookList/>, document.getElementById('root'))
